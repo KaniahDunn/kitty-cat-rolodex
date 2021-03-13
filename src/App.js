1add2e0b353d8d19ec947074fdf6ddaaf93f722b
Kaniah Dunn <kaniahdunn@gmail.com>
2021-03-13 18:43:47 -0500

    calling to API and using componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: 'string1'
-          
-        },
-        {
-          name: "Dracula",
-          id: 'string2'
-        },
-        {
-          name: "Zombie",
-          id: "string3"
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
   }
 
   render() {
@@ -34,25 +25,5 @@ class App extends Component {
     )
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           test
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
