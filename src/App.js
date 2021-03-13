eb006cac42d0a1ed0d260920028e92f0686c5d8b
Kaniah Dunn <kaniahdunn@gmail.com>
2021-03-13 18:14:38 -0500

    initial monster loading with array and h1 element

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {
-      string: "Hello Kaniah"
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: 'string1'
+          
+        },
+        {
+          name: "Dracula",
+          id: 'string2'
+        },
+        {
+          name: "Zombie",
+          id: "string3"
+        }
+      ]
     }
   }
-  
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={() => this.setState({ string: "Hello Jayden"})}>Change Text </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+        }
+      </div>
+    )
+  }
 }
 // function App() {
 //   return (
