2fc59a92928ba34f60821cdb4e345cc3a03476ee
Kaniah Dunn <kaniahdunn@gmail.com>
2021-03-14 12:38:33 -0400

    adding filtering to search field

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(
+        searchField.toLowerCase()
+      )
+    )
     return (
       <div className="App">
         <input
@@ -32,7 +38,7 @@ class App extends Component {
           }
         />
         <CardList
-          monsters={this.state.monsters}>
+          monsters={filteredMonsters}>
         </CardList>
       </div>
     )
